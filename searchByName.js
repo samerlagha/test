6458cd1dca9ef2d5b9b52aa667d218740136392e
samerlagha <samer_la@mail.ru>
2019-09-12 09:22:45 +0300

    Update searchByName.js

--- searchByName.js
+++ searchByName.js
@@ -12,7 +12,7 @@ function searchByName2(users, userName)
         return user.name.indexOf(userName) != -1;
     });
 }
-
+  console.log( searchByName2(arr,'Andrew'));
 //const searchByName = (users, userName) => users.filter(user => user.name === userName);
 const searchByName = function(users, userName) {
   return users.filter(user => user.name === userName);
